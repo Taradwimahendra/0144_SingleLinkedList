dcce710b52184debb6b1f2d03a7311d963faf679
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-05-08 15:45:06 +0700

    menggunakan peulangan while

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -37,5 +37,23 @@ class List {
             START = nodeBaru;
             return;
         }
+
+        Node *previous = START;
+        Node *current = START;
+
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi  noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
 };
